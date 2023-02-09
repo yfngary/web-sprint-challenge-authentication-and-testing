8d290be07749e9bdbc7964226b76c092e8bc0da9
yfngary <kalebglundquist@gmail.com>
2023-02-09 14:31:23 -0700

    wip

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -1,19 +1,21 @@
-const jwt = require('jsonwebtoken')
 const JWT_SECRET = require('../secrets/index')
+
+const jwt = require('jsonwebtoken')
+
 module.exports = (req, res, next) => {
   const token = req.headers.authorization
-  if(!token) {
-    return next({status: 401, message: 'Token required'})
+  if (!token) {
+    return next({ status: 401, message: 'token required' })
   }
 
   jwt.verify(token, JWT_SECRET, (err, decodedToken) => {
-    if(err){
-      res.status(401).json({message: "token invalid"})
+    if(err) {
+      next({status: 401, message: 'token invalid'})
     } else {
       req.decodedToken = decodedToken
       next()
     }
-  }) 
+  })
   /*
     IMPLEMENT
 
