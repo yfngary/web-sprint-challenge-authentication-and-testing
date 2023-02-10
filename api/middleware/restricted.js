d2ece6d0980d80c4ef8c8a00f88dfaad12228aef
yfngary <kalebglundquist@gmail.com>
2023-02-09 19:31:35 -0700

    wip

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -5,7 +5,7 @@ const jwt = require('jsonwebtoken')
 module.exports = (req, res, next) => {
   const token = req.headers.authorization
   if (!token) {
-    return next({ status: 401, message: 'token required' })
+    res.status(401).json({message: "token required"})
   }
 
   jwt.verify(token, JWT_SECRET, (err, decodedToken) => {
